71a390d3726a2eaa7e64de2358c0e543cd15aa0c
Guilherme <guilherme.s.goncalves@outlook.com>
2023-05-29 00:16:04 -0300

    Alert component

---
+++ src/components/alerts/MessageAlert.tsx
@@ -0,0 +1,42 @@
+import { useState } from 'react';
+import Snackbar, { SnackbarOrigin } from '@mui/material/Snackbar';
+import { Alert } from '@mui/material';
+
+type SnackbarProps = {
+    alertType: string;
+    message: string;
+};
+
+const MessageAlert = ({ alertType, message }: SnackbarProps) => {
+    const [open, setOpen] = useState(true);
+
+    const handleClose = () => {
+        setOpen(false);
+    };
+
+    const snackbarOrigin: SnackbarOrigin = {
+        vertical: 'bottom',
+        horizontal: 'center',
+    };
+
+    return (
+        <Snackbar
+            open={open}
+            autoHideDuration={5000}
+            anchorOrigin={snackbarOrigin}
+            onClose={handleClose}
+        >
+            {alertType === 'error' ? (
+                <Alert severity="error" onClose={handleClose}>
+                    {message}
+                </Alert>
+            ) : (
+                <Alert severity="success" onClose={handleClose}>
+                    {message}
+                </Alert>
+            )}
+        </Snackbar>
+    );
+};
+
+export default MessageAlert;
--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -5,12 +5,15 @@ import TaskForm from '../components/form/TaskForm';
 import MyCustomButttom from '../components/buttom/CustomButtom';
 import AddIcon from '@mui/icons-material/Add';
 import api from '../services/axiosConfig';
+import MessageAlert from '../components/alerts/MessageAlert';
 
 const Home = () => {
     const [loading, setLoading] = useState(false);
     const [showModal, setShowModal] = useState(false);
     const [error, setError] = useState(false);
     const [inputValue, setInputValue] = useState('');
+    const [message, setMessage] = useState('');
+    const [messageType, setMessageType] = useState('');
 
     useEffect(() => {
         setLoading(false);
@@ -21,6 +24,7 @@ const Home = () => {
     };
 
     const handleSubmit = async (value: string) => {
+        setLoading(true);
         if (!value) {
             setError(true);
             return;
@@ -31,11 +35,16 @@ const Home = () => {
         await api
             .post('/tarefas', { titulo: value })
             .then((res) => {
-                console.log(res.data);
+                setMessage(res.data.msg);
+                setMessageType('success');
                 setInputValue('');
+                setLoading(false);
+                setShowModal(false);
             })
             .catch((res) => {
-                console.log(res.data);
+                setLoading(false);
+                setMessage(res.data.msg);
+                setMessageType('error');
             });
     };
 
@@ -52,6 +61,7 @@ const Home = () => {
                             startIcon={<AddIcon />}
                         />
                     </div>
+                    <MessageAlert alertType={messageType} message={message} />
                     <DialogModal
                         isOpen={showModal}
                         onClose={() => setShowModal(false)}
